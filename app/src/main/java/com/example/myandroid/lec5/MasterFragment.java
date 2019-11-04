b27aacc28a501dca09766d66fa8c4a8e5ea18074
akrushinskiy <a.krushinskiy@evrius.com>
2019-11-04 19:22:48 +0200

    refactor adapter

--- app/src/main/java/com/example/myandroid/lec5/MasterFragment.java
+++ app/src/main/java/com/example/myandroid/lec5/MasterFragment.java
@@ -4,7 +4,6 @@ package com.example.myandroid.lec5;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,27 +12,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.myandroid.R;
 
-public class MasterFragment extends Fragment implements View.OnClickListener {
+public class MasterFragment extends Fragment implements MyAdapter.IMyAdapter {
 
     interface IMainFragment{
         void onItemClick(int position);
     }
 
     public static MasterFragment newInstance() {
-        
         Bundle args = new Bundle();
-        
         MasterFragment fragment = new MasterFragment();
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public MasterFragment() {
-
     }
 
     RecyclerView recyclerView;
@@ -48,8 +40,11 @@ public class MasterFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_master, container, false);
+
         recyclerView = view.findViewById(R.id.list);
-        MasterFragment.MyAdapter adapter = new MasterFragment.MyAdapter();
+        MyAdapter adapter = new MyAdapter();
+        adapter.setOnItemClickListener(this);
+
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
@@ -65,50 +60,11 @@ public class MasterFragment extends Fragment implements View.OnClickListener {
         return view;
     }
 
+
     @Override
-    public void onClick(View v) {
-        int itemPosition = (int) v.getTag();
+    public void onItemClick(int position) {
         if (null != listener) {
-            listener.onItemClick(itemPosition);
-        }
-
-    }
-
-    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            TextView text1, text2;
-
-            public ViewHolder(@NonNull View itemView) {
-                super(itemView);
-                text1 = itemView.findViewById(android.R.id.text1);
-                text2 = itemView.findViewById(android.R.id.text2);
-            }
-
-            void bind(BankCardModel bankCard, int position) {
-                text2.setText(bankCard.getOwnerName());
-                text1.setText(bankCard.getNum());
-                itemView.setTag(position);
-                itemView.setOnClickListener(MasterFragment.this);
-            }
-        }
-
-        @NonNull
-        @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_2, parent, false);
-            return new MyAdapter.ViewHolder(v);
-
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
-            holder.bind(BankCardManager.getCard(position), position);
-        }
-
-        @Override
-        public int getItemCount() {
-            return BankCardManager.getCountCard();
+            listener.onItemClick(position);
         }
     }
 
